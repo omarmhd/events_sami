63f96cf649fbe50d28b25f7bf4fd552c744e6180
omarmhd <omarmhd19988@gmail.com>
2026-01-22 10:22:57 +0200

    t

--- app/Exports/TicketExport.php
+++ app/Exports/TicketExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-use App\Models\Ticket;
+use App\Models\EventInvitation;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
